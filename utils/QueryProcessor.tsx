d62a98664ba3c21e0f1ef3a347be1d526ac4e7f4
Leo Junyi Liu <junyili2@andrew.cmu.edu>
2023-03-14 16:15:28 -0400

    wwef

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -41,6 +41,50 @@ export default function QueryProcessor(query: string): string {
 
   }
 
+
+
+  if (query.toLowerCase().includes("hich of the following numbers is both a square and a cube")) {
+
+
+
+    const a = query.split(":");
+    const c = a[1].substring(1, a[1].length - 1);
+    const b = c.split(", ");
+
+    const i = parseInt(b[0]);
+    const ii = parseInt(b[1]);
+    const iii = parseInt(b[2]);
+    const i4 = parseInt(b[3]);
+    const i5 = parseInt(b[4]);
+    const i6 = parseInt(b[5]);
+    const i7 = parseInt(b[6]);
+
+    if(Math.sqrt(i) * Math.sqrt(i) == i && Math.cbrt(i)*Math.cbrt(i)*Math.cbrt(i) == i){
+      return i + "";
+    }
+    if(Math.sqrt(ii) * Math.sqrt(ii) == ii && Math.cbrt(ii)*Math.cbrt(ii)*Math.cbrt(ii) == ii){
+      return ii + "";
+    }
+    if(Math.sqrt(iii) * Math.sqrt(iii) == iii && Math.cbrt(iii)*Math.cbrt(iii)*Math.cbrt(iii) == iii){
+      return iii + "";
+    }
+    if(Math.sqrt(i4) * Math.sqrt(i4) == i4 && Math.cbrt(i4)*Math.cbrt(i4)*Math.cbrt(i4) == i4){
+      return i4 + "";
+    }
+    if(Math.sqrt(i5) * Math.sqrt(i5) == i5 && Math.cbrt(i5)*Math.cbrt(i5)*Math.cbrt(i5) == i5){
+      return i5 + "";
+    }
+    if(Math.sqrt(i6) * Math.sqrt(i6) == i6 && Math.cbrt(i6)*Math.cbrt(i6)*Math.cbrt(i6) == i6){
+      return i6 + "";
+    }
+    if(Math.sqrt(i7) * Math.sqrt(i7) == i7 && Math.cbrt(i7)*Math.cbrt(i7)*Math.cbrt(i7) == i7){
+      return i7 + "";
+    }
+
+  }
+
+
+
   return "";
 
   if (query.toLowerCase().includes("what is your name?")) {
